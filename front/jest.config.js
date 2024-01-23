2d8a1ea698aa681ce134e019231a30b53251f5b5
Clément Trotignon <ctrotignon@kameleoon.com>
2024-01-23 20:45:28 +0100

    updating packages and jest config front

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,9 +1,21 @@
+// module.exports = {
+// 	preset: 'jest-expo',
+// 	setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+// 	transform: {
+// 		'^.+\\.tsx?$': 'babel-jest',
+// 	},
+// 	moduleNameMapper: {
+// 		'/\\.(png|jpg|jpeg|gif|svg)$/': '../front/mocks/fileMock.js',
+// 	},
+// };
+
 module.exports = {
 	preset: 'jest-expo',
 	setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 	transform: {
 		'^.+\\.tsx?$': 'babel-jest',
 	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	moduleNameMapper: {
 		'/\\.(png|jpg|jpeg|gif|svg)$/': '../front/mocks/fileMock.js',
 	},
